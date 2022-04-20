8e06d084b6bf47367d7e9b31f10c1ef2802d1811
replaceitem <40722305+replaceitem@users.noreply.github.com>
2022-04-20 18:18:09 +0200

    Added symbol panel to book edit screen

---
+++ src/main/java/symbolchat/mixin/BookEditScreenMixin.java
@@ -0,0 +1,60 @@
+package symbolchat.mixin;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.BookEditScreen;
+import net.minecraft.client.util.SelectionManager;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import symbolchat.SymbolInsertable;
+import symbolchat.gui.SymbolSelectionPanel;
+import symbolchat.gui.widget.symbolButton.OpenSymbolPanelButtonWidget;
+import symbolchat.gui.widget.symbolButton.SymbolButtonWidget;
+
+@Mixin(BookEditScreen.class)
+public class BookEditScreenMixin extends Screen implements SymbolInsertable {
+    @Shadow @Final private SelectionManager currentPageSelectionManager;
+    private SymbolButtonWidget symbolButtonWidget;
+    private SymbolSelectionPanel symbolSelectionPanel;
+
+    protected BookEditScreenMixin(Text title) {
+        super(title);
+    }
+
+    @Inject(method = "init", at = @At(value = "RETURN"))
+    private void addSymbolButton(CallbackInfo ci) {
+        int symbolButtonX = this.width-SymbolButtonWidget.symbolSize;
+        int symbolButtonY = this.height-2-SymbolButtonWidget.symbolSize;
+        this.symbolSelectionPanel = new SymbolSelectionPanel(this,this.width-SymbolSelectionPanel.width-2,symbolButtonY-2-SymbolSelectionPanel.height);
+        this.symbolButtonWidget = new OpenSymbolPanelButtonWidget(this, symbolButtonX, symbolButtonY, this.symbolSelectionPanel);
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        if(symbolSelectionPanel.mouseClicked(mouseX,mouseY,button)) return true;
+        if(symbolButtonWidget.mouseClicked(mouseX,mouseY,button)) return true;
+        return super.mouseClicked(mouseX,mouseY,button);
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double amount) {
+        if(symbolSelectionPanel.mouseScrolled(mouseX,mouseY,amount)) return true;
+        return super.mouseScrolled(mouseX, mouseY, amount);
+    }
+
+    @Inject(method = "render", at = @At(value = "RETURN"))
+    private void renderSymbolButton(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        symbolButtonWidget.render(matrices,mouseX,mouseY,delta);
+        symbolSelectionPanel.render(matrices, mouseX, mouseY, delta);
+    }
+
+    @Override
+    public void insertSymbol(String symbol) {
+        this.currentPageSelectionManager.insert(symbol);
+    }
+}
