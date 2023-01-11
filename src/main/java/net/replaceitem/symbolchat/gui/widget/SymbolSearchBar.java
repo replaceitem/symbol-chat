46ad37f23c5e9ebb54ea341f0f83c91ea860ef1e
replaceitem <40722305+replaceitem@users.noreply.github.com>
2023-01-11 18:31:40 +0100

    Added hint to search bar

--- src/main/java/net/replaceitem/symbolchat/gui/widget/SymbolSearchBar.java
+++ src/main/java/net/replaceitem/symbolchat/gui/widget/SymbolSearchBar.java
@@ -8,6 +8,8 @@ import net.minecraft.text.Text;
 
 public class SymbolSearchBar extends TextFieldWidget {
 
+    public static final Text HINT_TEXT = Text.translatable("symbolchat.search");
+
     public SymbolSearchBar(int x, int y, int width, int height) {
         super(MinecraftClient.getInstance().textRenderer, x, y, width, height, Text.empty());
         this.setDrawsBackground(false);
@@ -19,7 +21,12 @@ public class SymbolSearchBar extends TextFieldWidget {
         matrices.push();
         matrices.translate(0.0, 0.0, 200.0f);
 
+        if(this.getText().isEmpty()) {
+            MinecraftClient.getInstance().textRenderer.drawWithShadow(matrices, HINT_TEXT, this.getX(), this.getY(), 0xa0a0a0a0);
+        }
+
         super.render(matrices, mouseX, mouseY, delta);
+
         int lineY = this.getY() + this.height - 1;
         fill(matrices, this.getX(), lineY, this.getX() + this.width - 1, lineY+1, this.isActive() || this.isHovered() ? 0x99FFFFFF : 0x99A0A0A0);
 
