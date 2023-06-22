a8e74ccb6513652f0bd15fa98a97b98c522bc4b2
replaceitem <40722305+replaceitem@users.noreply.github.com>
2023-06-22 23:04:19 +0200

    Added back outline to unicode table screen

--- src/main/java/net/replaceitem/symbolchat/gui/widget/symbolButton/OpenSymbolPanelButtonWidget.java
+++ src/main/java/net/replaceitem/symbolchat/gui/widget/symbolButton/OpenSymbolPanelButtonWidget.java
@@ -1,6 +1,5 @@
 package net.replaceitem.symbolchat.gui.widget.symbolButton;
 
-import net.minecraft.client.gui.DrawContext;
 import net.replaceitem.symbolchat.gui.SymbolSelectionPanel;
 
 public class OpenSymbolPanelButtonWidget extends SymbolButtonWidget {
@@ -21,13 +20,5 @@ public class OpenSymbolPanelButtonWidget extends SymbolButtonWidget {
     @Override
     public boolean isSelected() {
         return super.isSelected() || symbolSelectionPanel.visible;
-    }
-
-    @Override
-    protected void drawOutline(DrawContext drawContext) {
-        drawContext.drawHorizontalLine(this.getX()-1, this.getX()+width, this.getY()-1, 0xFFFFFFFF);
-        drawContext.drawVerticalLine(this.getX()-1, this.getY()-1, this.getY()+height, 0xFFFFFFFF);
-        drawContext.drawHorizontalLine(this.getX()-1, this.getX()+width, this.getY()+height, 0xFFFFFFFF);
-        drawContext.drawVerticalLine(this.getX()+width, this.getY()-1, this.getY()+height, 0xFFFFFFFF);
     }
 }
--- src/main/java/net/replaceitem/symbolchat/gui/widget/symbolButton/SymbolButtonWidget.java
+++ src/main/java/net/replaceitem/symbolchat/gui/widget/symbolButton/SymbolButtonWidget.java
@@ -93,14 +93,17 @@ public abstract class SymbolButtonWidget extends ClickableWidget implements Draw
 
             drawContext.drawCenteredTextWithShadow(textRenderer, this.getMessage(), this.getX() + this.width / 2, this.getY() + (this.height - 8) / 2, argb);
             
-            if(this.isSelected() || this.isFocused()) {
+            if(this.isSelected()) {
                 this.drawOutline(drawContext);
             }
         }
     }
     
     protected void drawOutline(DrawContext drawContext) {
-
+        drawContext.drawHorizontalLine(this.getX()-1, this.getX()+width, this.getY()-1, 0xFFFFFFFF);
+        drawContext.drawVerticalLine(this.getX()-1, this.getY()-1, this.getY()+height, 0xFFFFFFFF);
+        drawContext.drawHorizontalLine(this.getX()-1, this.getX()+width, this.getY()+height, 0xFFFFFFFF);
+        drawContext.drawVerticalLine(this.getX()+width, this.getY()-1, this.getY()+height, 0xFFFFFFFF);
     }
 
     @Override
